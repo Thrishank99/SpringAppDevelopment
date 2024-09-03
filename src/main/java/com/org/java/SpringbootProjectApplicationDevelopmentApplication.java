e3f4d94ff06dc5835003045d03e6f22370b69cd7
Thrishank99 <thrishank966@gmail.com>
2024-09-03 11:40:56 +0530

    refdsds

--- src/main/java/com/org/java/SpringbootProjectApplicationDevelopmentApplication.java
+++ src/main/java/com/org/java/SpringbootProjectApplicationDevelopmentApplication.java
@@ -2,9 +2,16 @@ package com.org.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootProjectApplicationDevelopmentApplication {
+public class SpringbootProjectApplicationDevelopmentApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootProjectApplicationDevelopmentApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProjectApplicationDevelopmentApplication.class, args);
